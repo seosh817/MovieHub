8a24fa190249a51a28a5118b2b25d52052d933c9
seunghwan <seosh817@naver.com>
2023-11-29 16:17:03 +0900

    feat: Add io dispatcher

--- core/domain/src/main/kotlin/com/seosh817/moviehub/core/domain/usecase/movies/GetTopRatedMoviesUseCase.kt
+++ core/domain/src/main/kotlin/com/seosh817/moviehub/core/domain/usecase/movies/GetTopRatedMoviesUseCase.kt
@@ -1,14 +1,21 @@
 package com.seosh817.moviehub.core.domain.usecase.movies
 
 import androidx.paging.PagingData
+import com.seosh817.moviehub.core.common.network.Dispatcher
+import com.seosh817.moviehub.core.common.network.MovieHubDispatchers
 import com.seosh817.moviehub.core.domain.repository.MovieRepository
 import com.seosh817.moviehub.core.model.Movie
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetTopRatedMoviesUseCase @Inject constructor(
-    private val movieRepository: MovieRepository
+    private val movieRepository: MovieRepository,
+    @Dispatcher(MovieHubDispatchers.IO) private val dispatcher: CoroutineDispatcher
 ) : GetMoviesUseCase {
 
-    override operator fun invoke(language: String): Flow<PagingData<Movie>> = movieRepository.fetchTopRatedMovies(language)
+    override operator fun invoke(language: String): Flow<PagingData<Movie>> = movieRepository
+        .fetchTopRatedMovies(language)
+        .flowOn(dispatcher)
 }
--- core/domain/src/main/kotlin/com/seosh817/moviehub/core/domain/usecase/movies/GetUpcomingMoviesUseCase.kt
+++ core/domain/src/main/kotlin/com/seosh817/moviehub/core/domain/usecase/movies/GetUpcomingMoviesUseCase.kt
@@ -1,14 +1,21 @@
 package com.seosh817.moviehub.core.domain.usecase.movies
 
 import androidx.paging.PagingData
+import com.seosh817.moviehub.core.common.network.Dispatcher
+import com.seosh817.moviehub.core.common.network.MovieHubDispatchers
 import com.seosh817.moviehub.core.domain.repository.MovieRepository
 import com.seosh817.moviehub.core.model.Movie
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetUpcomingMoviesUseCase @Inject constructor(
-    private val movieRepository: MovieRepository
-) : GetMoviesUseCase{
+    private val movieRepository: MovieRepository,
+    @Dispatcher(MovieHubDispatchers.IO) private val dispatcher: CoroutineDispatcher
+) : GetMoviesUseCase {
 
-    override operator fun invoke(language: String): Flow<PagingData<Movie>> = movieRepository.fetchUpcomingMovies(language)
+    override operator fun invoke(language: String): Flow<PagingData<Movie>> = movieRepository
+        .fetchUpcomingMovies(language)
+        .flowOn(dispatcher)
 }
