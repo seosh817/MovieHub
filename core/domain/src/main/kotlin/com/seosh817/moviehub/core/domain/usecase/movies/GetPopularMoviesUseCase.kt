a0175649b6d239143ff1ef565ae5c578a98c3c98
seunghwan <seosh817@naver.com>
2023-11-29 16:17:03 +0900

    feat: Add io dispatcher

--- core/domain/src/main/kotlin/com/seosh817/moviehub/core/domain/usecase/movies/GetPopularMoviesUseCase.kt
+++ core/domain/src/main/kotlin/com/seosh817/moviehub/core/domain/usecase/movies/GetPopularMoviesUseCase.kt
@@ -1,14 +1,22 @@
 package com.seosh817.moviehub.core.domain.usecase.movies
 
 import androidx.paging.PagingData
+import com.seosh817.moviehub.core.common.network.Dispatcher
+import com.seosh817.moviehub.core.common.network.MovieHubDispatchers
 import com.seosh817.moviehub.core.domain.repository.MovieRepository
 import com.seosh817.moviehub.core.model.Movie
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class GetPopularMoviesUseCase @Inject constructor(
-    private val movieRepository: MovieRepository
+    private val movieRepository: MovieRepository,
+    @Dispatcher(MovieHubDispatchers.IO) private val dispatcher: CoroutineDispatcher
 ) : GetMoviesUseCase {
 
-    override operator fun invoke(language: String): Flow<PagingData<Movie>> = movieRepository.fetchPopularMovies(language)
+    override operator fun invoke(language: String): Flow<PagingData<Movie>> = movieRepository
+        .fetchPopularMovies(language)
+        .flowOn(dispatcher)
 }
