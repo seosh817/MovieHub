69f7f9cc3ac9d384f09a7e58e6841112f5e7f844
seunghwan <seosh817@naver.com>
2023-12-03 00:25:41 +0900

    fix: Fixed circular dependency bug

--- core/data/src/main/kotlin/com/seosh817/moviehub/core/data/source/MovieDataSource.kt
+++ core/data/src/main/kotlin/com/seosh817/moviehub/core/data/source/MovieDataSource.kt
@@ -1,22 +1,22 @@
 package com.seosh817.moviehub.core.data.source
 
 import com.seosh817.common.result.ResultState
-import com.seosh817.moviehub.core.network.model.movie_list.MovieResponseEntity
+import com.seosh817.moviehub.core.model.MovieResponse
 
 interface MovieDataSource {
 
     suspend fun fetchPopularMovies(
         page: Int,
         language: String
-    ): ResultState<MovieResponseEntity>
+    ): ResultState<MovieResponse>
 
     suspend fun fetchTopRatedMovies(
         page: Int,
         language: String
-    ): ResultState<MovieResponseEntity>
+    ): ResultState<MovieResponse>
 
     suspend fun fetchUpcomingMovies(
         page: Int,
         language: String
-    ): ResultState<MovieResponseEntity>
+    ): ResultState<MovieResponse>
 }
