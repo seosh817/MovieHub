1be8aa87d3dfa2ceeb31cda18decc603ee5158eb
seunghwan <seosh817@naver.com>
2023-12-03 00:25:41 +0900

    fix: Fixed circular dependency bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.secrets) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
