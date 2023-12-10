354d8eddad4deea2ddfd1fcd19ddde2a21105f7e
seunghwan <seosh817@naver.com>
2023-12-10 14:50:32 +0900

    feat: Add MainTopAppBar to MainActivity

--- feature/movies/src/main/kotlin/com/seosh817/moviehub/feature/movies/navigation/MoviesScreen.kt
+++ feature/movies/src/main/kotlin/com/seosh817/moviehub/feature/movies/navigation/MoviesScreen.kt
@@ -1,5 +1,7 @@
 package com.seosh817.moviehub.feature.movies.navigation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,7 +13,6 @@ internal fun MoviesRoute(
     modifier: Modifier = Modifier,
     viewModel: MoviesViewModel = hiltViewModel(),
 ) {
-
     MoviesScreen(
         onMovieClick = onMovieClick,
         modifier = modifier,
@@ -23,5 +24,7 @@ fun MoviesScreen(
     onMovieClick: (Long) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Text("Movie Screen")
+    Column(modifier = modifier.fillMaxSize()) {
+        Text(text = "Movies")
+    }
 }
