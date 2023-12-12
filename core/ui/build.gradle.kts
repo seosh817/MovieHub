612c63eedb9b201721b57642c07aa81bdaabdd9c
seunghwan <seosh817@naver.com>
2023-12-12 22:47:09 +0900

    feat: Add core ui module

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,35 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.moviehub.android.library)
+    alias(libs.plugins.moviehub.android.library.compose)
+}
+
+android {
+    namespace = "com.seosh817.moviehub.core.ui"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.foundation.layout)
+    api(libs.androidx.compose.material.iconsExtended)
+    api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.runtime)
+    api(libs.androidx.compose.runtime.livedata)
+    api(libs.androidx.compose.ui.tooling.preview)
+    api(libs.androidx.compose.ui.util)
+    api(libs.androidx.constraintlayout.compose)
+
+    debugApi(libs.androidx.compose.ui.tooling)
+
+    implementation(projects.core.designsystem)
+    implementation(projects.core.domain)
+    implementation(projects.core.model)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.compose)
+}
