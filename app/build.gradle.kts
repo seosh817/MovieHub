f48997dab896c492f8eeb8baacc8d7845747a3e8
seunghwan <seosh817@naver.com>
2023-12-25 23:48:35 +0900

    feat: Add movie detail feature module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 
 dependencies {
     implementation(projects.feature.movies)
+    implementation(projects.feature.movieDetail)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
