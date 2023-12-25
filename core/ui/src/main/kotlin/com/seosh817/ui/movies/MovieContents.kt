35385bbf04cd5b25ed7871240322fd12baf30243
seunghwan <seosh817@naver.com>
2023-12-25 20:21:15 +0900

    feat: Add Swipe to refresh

--- core/ui/src/main/kotlin/com/seosh817/ui/movies/MovieContents.kt
+++ core/ui/src/main/kotlin/com/seosh817/ui/movies/MovieContents.kt
@@ -2,19 +2,24 @@ package com.seosh817.ui.movies
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.PullRefreshState
+import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -26,10 +31,14 @@ import com.seosh817.moviehub.core.designsystem.theme.Dimens
 import com.seosh817.moviehub.core.model.Movie
 import com.seosh817.moviehub.core.ui.R
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MovieContents(
+    isRefreshing: Boolean,
     moviePagingItems: LazyPagingItems<Movie>,
     lazyGridState: LazyGridState,
+    pullRefreshModifier: Modifier,
+    pullRefreshState: PullRefreshState,
     onMovieClick: (Long) -> Unit,
     errorText: @Composable (String) -> Unit,
 ) {
@@ -51,41 +60,54 @@ fun MovieContents(
         }
 
         else -> {
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                state = lazyGridState,
-                contentPadding = PaddingValues(horizontal = Dimens.dp_4, vertical = Dimens.dp_4),
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .pullRefresh(pullRefreshState)
             ) {
-                items(
-                    count = moviePagingItems.itemCount,
-                    key = moviePagingItems.itemKey(),
-                    contentType = moviePagingItems.itemContentType()
-                ) { index ->
-                    val movie: Movie? = moviePagingItems[index]
-                    if (movie != null) {
-                        MovieContentItem(
-                            context = context,
-                            movie = movie,
-                            noImageText = {
-                                Text(
-                                    text = stringResource(R.string.no_image),
-                                    style = MaterialTheme.typography.bodyLarge.copy(
-                                        color = MaterialTheme.colorScheme.secondary
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    state = lazyGridState,
+                    contentPadding = PaddingValues(horizontal = Dimens.dp_4, vertical = Dimens.dp_4),
+                ) {
+                    items(
+                        count = moviePagingItems.itemCount,
+                        key = moviePagingItems.itemKey(),
+                        contentType = moviePagingItems.itemContentType()
+                    ) { index ->
+                        val movie: Movie? = moviePagingItems[index]
+                        if (movie != null) {
+                            MovieContentItem(
+                                context = context,
+                                movie = movie,
+                                noImageText = {
+                                    Text(
+                                        text = stringResource(R.string.no_image),
+                                        style = MaterialTheme.typography.bodyLarge.copy(
+                                            color = MaterialTheme.colorScheme.secondary
+                                        )
                                     )
-                                )
-                            },
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .height(240.dp)
-                                .padding(Dimens.dp_4)
-                                .clip(MaterialTheme.shapes.small)
-                                .background(MaterialTheme.colorScheme.background)
-                                .clickable {
-                                    onMovieClick.invoke(movie.id)
-                                }
-                        )
+                                },
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .height(240.dp)
+                                    .padding(Dimens.dp_4)
+                                    .clip(MaterialTheme.shapes.small)
+                                    .background(MaterialTheme.colorScheme.background)
+                                    .clickable {
+                                        onMovieClick.invoke(movie.id)
+                                    }
+                            )
+                        }
                     }
                 }
+                PullRefreshIndicator(
+                    refreshing = isRefreshing,
+                    state = pullRefreshState,
+                    modifier = pullRefreshModifier,
+                    backgroundColor = Color.White,
+                    contentColor = MaterialTheme.colorScheme.primary
+                )
             }
         }
     }
