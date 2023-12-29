b7dec6284b31fc3b44c3186cf188c7bc7e37bb1e
seunghwan <seosh817@naver.com>
2023-12-29 23:46:35 +0900

    feat: Add fetch movieDetail data

--- core/data/src/main/kotlin/com/seosh817/moviehub/core/data/repository/MovieRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/seosh817/moviehub/core/data/repository/MovieRepositoryImpl.kt
@@ -3,9 +3,12 @@ package com.seosh817.moviehub.core.data.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import com.seosh817.common.result.ResultState
+import com.seosh817.common.result.extension.map
 import com.seosh817.moviehub.core.data.paging.MovieListPagingSource
 import com.seosh817.moviehub.core.data.source.MovieDataSource
 import com.seosh817.moviehub.core.domain.repository.MovieRepository
+import com.seosh817.moviehub.core.model.MovieDetail
 import com.seosh817.moviehub.core.model.MovieOverview
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -32,6 +35,10 @@ class MovieRepositoryImpl @Inject constructor(
         return createPager(MovieListPagingSource {
             movieDataSource.fetchUpcomingMovies(it, language)
         })
+    }
+
+    override suspend fun fetchMovieDetail(movieId: Long, language: String?): ResultState<MovieDetail> {
+        return movieDataSource.fetchMovieDetail(movieId, language)
     }
 
     private fun createPager(movieListPagingSource: MovieListPagingSource): Flow<PagingData<MovieOverview>> {
