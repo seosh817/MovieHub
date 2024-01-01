335ca2129caad1648ebaf6f1649ccb5c69a639ef
seunghwan <seosh817@naver.com>
2024-01-01 22:57:11 +0900

    feature: Add MovieDetailScreen

--- feature/movie_detail/src/main/kotlin/com/seosh817/moviehub/feature/movie_detail/MovieDetailUiState.kt
+++ feature/movie_detail/src/main/kotlin/com/seosh817/moviehub/feature/movie_detail/MovieDetailUiState.kt
@@ -5,5 +5,5 @@ import com.seosh817.moviehub.core.model.MovieDetail
 sealed interface MovieDetailUiState {
     data object Loading : MovieDetailUiState
     data class Success(val movieDetail: MovieDetail) : MovieDetailUiState
-    data class Error(val throwable: Throwable) : MovieDetailUiState
+    data class Error(val e: Throwable) : MovieDetailUiState
 }
