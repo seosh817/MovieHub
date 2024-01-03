a28102728dbbb6c265be98de4e4936252ac7fbbd
seunghwan <seosh817@naver.com>
2024-01-03 18:43:03 +0900

    feat: Apply edgeToEdge

--- core/designsystem/src/main/kotlin/com/seosh817/moviehub/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/seosh817/moviehub/core/designsystem/theme/Theme.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -110,8 +111,8 @@ fun MovieHubTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
