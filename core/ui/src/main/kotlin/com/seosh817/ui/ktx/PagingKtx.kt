379505a42a665c4753dcd79d94597fe0a755c49e
seunghwan <seosh817@naver.com>
2024-01-06 00:37:04 +0900

    refactor: Remove unused functions

--- core/ui/src/main/kotlin/com/seosh817/ui/ktx/PagingKtx.kt
+++
@@ -1,7 +0,0 @@
-package com.seosh817.ui.ktx
-
-import androidx.paging.LoadState
-import androidx.paging.compose.LazyPagingItems
-
-val <T : Any> LazyPagingItems<T>.isError: Boolean
-    get() = loadState.refresh is LoadState.Error || loadState.append is LoadState.Error
