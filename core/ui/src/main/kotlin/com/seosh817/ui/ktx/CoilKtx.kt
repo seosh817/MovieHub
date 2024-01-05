abf7ce31a73b75d62944ae2838b08b9984c2db3f
seunghwan <seosh817@naver.com>
2024-01-06 00:37:04 +0900

    refactor: Remove unused functions

--- core/ui/src/main/kotlin/com/seosh817/ui/ktx/CoilKtx.kt
+++
@@ -1,9 +0,0 @@
-package com.seosh817.ui.ktx
-
-import coil.compose.AsyncImagePainter
-
-val AsyncImagePainter.State.isErrorOrEmpty: Boolean
-    get() = this is AsyncImagePainter.State.Error || this is AsyncImagePainter.State.Empty
-
-val AsyncImagePainter.State.isLoading: Boolean
-    get() = this is AsyncImagePainter.State.Loading
