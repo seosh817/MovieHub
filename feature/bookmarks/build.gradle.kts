bb0f6e40f2ea5f591227cad6ed844ab4ed6b906a
seunghwan <seosh817@naver.com>
2024-01-07 23:16:45 +0900

    feat(bookmarks): Add Bookmarks screen

---
+++ feature/bookmarks/build.gradle.kts
@@ -0,0 +1,19 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.moviehub.android.feature)
+    alias(libs.plugins.moviehub.android.library.compose)
+}
+
+android {
+    namespace = "com.seosh817.moviehub.feature.bookmarks"
+}
+
+dependencies {
+    implementation(projects.common)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.appcompat)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
---
+++ feature/bookmarks/src/main/kotlin/com/seosh817/moviehub/feature/bookmarks/BookmarksScreen.kt
@@ -0,0 +1,8 @@
+package com.seosh817.moviehub.feature.bookmarks
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun BookmarksRoute() {
+
+}
