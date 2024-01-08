efacc839a6cdae50f86e5fedf25dfbe8a03f8b7f
seunghwan <seosh817@naver.com>
2024-01-08 18:04:02 +0900

    refactor: Change movie model id type

--- core/data/src/main/kotlin/com/seosh817/moviehub/core/data/source/AppStartUpPreferencesDataSource.kt
+++ core/data/src/main/kotlin/com/seosh817/moviehub/core/data/source/AppStartUpPreferencesDataSource.kt
@@ -12,5 +12,5 @@ interface AppStartUpPreferencesDataSource {
 
     suspend fun setDarkThemeMode(darkThemeMode: DarkThemeMode)
 
-    suspend fun setBookMarkedMovieIds(movieIds: Set<String>)
+    suspend fun setBookMarkedMovieIds(movieIds: Set<Long>)
 }
