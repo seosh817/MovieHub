b2a02806613f332feacf529c1859ff21f829a5d9
seunghwan <seosh817@naver.com>
2024-01-09 15:39:39 +0900

    refactor: Rename preferences properties/class

--- app/src/main/java/com/seosh817/moviehub/ui/MainUiState.kt
+++ app/src/main/java/com/seosh817/moviehub/ui/MainUiState.kt
@@ -1,8 +1,8 @@
 package com.seosh817.moviehub.ui
 
-import com.seosh817.moviehub.core.model.AppSettings
+import com.seosh817.moviehub.core.model.UserSettings
 
 sealed interface MainUiState {
-    data class Success(val appSettings: AppSettings) : MainUiState
+    data class Success(val userSettings: UserSettings) : MainUiState
     data object Loading : MainUiState
 }
