5caaa1c4b4f28e271592e68114451b7edf6d7680
seunghwan <seosh817@naver.com>
2024-01-11 15:59:15 +0900

    chore: Apply spotless gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.secrets) apply false
+    
 }
 true // Needed to make the Suppress annotation work for the plugins block
