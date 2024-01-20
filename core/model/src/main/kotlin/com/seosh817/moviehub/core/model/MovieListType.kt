079fd8dde32814450eb1f6851e1ffe604f05c5b5
seunghwan <seosh817@naver.com>
2024-01-21 00:49:23 +0900

    feat: Add MovieListType

---
+++ core/model/src/main/kotlin/com/seosh817/moviehub/core/model/MovieListType.kt
@@ -0,0 +1,8 @@
+package com.seosh817.moviehub.core.model
+
+enum class MovieListType(val value: String) {
+    POPULAR("popular"),
+    TOP_RATED("top_rated"),
+    UPCOMING("upcoming"),
+    NOW_PLAYING("now_playing")
+}
