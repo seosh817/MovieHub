1750452c781a412b3aa37707548333b57e2ef5b1
seunghwan <seosh817@naver.com>
2024-01-21 00:43:25 +0900

    fix: Add Scaffold content padding

--- app/src/main/java/com/seosh817/moviehub/ui/MainScreen.kt
+++ app/src/main/java/com/seosh817/moviehub/ui/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.icons.Icons
@@ -81,6 +82,7 @@ fun MainScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .padding(padding)
                 .consumeWindowInsets(padding)
                 .windowInsetsPadding(
                     WindowInsets.safeDrawing.only(
