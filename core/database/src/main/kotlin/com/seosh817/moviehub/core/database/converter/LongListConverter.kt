d0eb6a8c881a0c91235babbabacb4c0db2d03e77
seunghwan <seosh817@naver.com>
2024-01-21 12:12:16 +0900

    fix: Modify LongListConverter

--- core/database/src/main/kotlin/com/seosh817/moviehub/core/database/converter/LongListConverter.kt
+++ core/database/src/main/kotlin/com/seosh817/moviehub/core/database/converter/LongListConverter.kt
@@ -6,11 +6,27 @@ class LongListConverter {
 
     @TypeConverter
     fun fromLongList(longList: List<Long>?): String? {
-        return longList?.joinToString(",")
+        return longList?.joinToString(DELIMITER)
     }
 
     @TypeConverter
     fun toLongList(longListString: String?): List<Long>? {
-        return longListString?.split(",")?.map { it.toLong() }
+        return longListString?.let {
+            if (it.isNotEmpty()) {
+                it
+                    .split(DELIMITER)
+                    .map { str ->
+                        str
+                            .trim()
+                            .toLong()
+                    }
+            } else {
+                null
+            }
+        }
+    }
+
+    companion object {
+        private const val DELIMITER = ","
     }
 }
