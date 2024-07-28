5f132e3c0e2855eb465d2fa8bba4da6d36f8b5ea
seunghwan <seosh817@naver.com>
2024-07-28 23:39:04 +0900

    chore: Update version code name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 
     defaultConfig {
         applicationId = "com.seosh817.moviehub"
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
