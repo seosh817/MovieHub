df52b4fe40e68a35c4356d97cd6717c244f7f75f
seunghwan <seosh817@naver.com>
2024-09-21 20:47:44 +0900

    chore: Disable ktlint standard package-name rule

--- gradle/init.gradle.kts
+++ gradle/init.gradle.kts
@@ -29,7 +29,8 @@ rootProject {
                     .editorConfigOverride(
                         mapOf(
                             "ktlint_standard_no-wildcard-imports" to "disabled",
-                            "ktlint_standard_function-naming" to "disabled"
+                            "ktlint_standard_function-naming" to "disabled",
+                            "ktlint_standard_package-name" to "disabled"
                         ),
                     )
                 licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
