335a2ae7514ab61975679a20dd94e287823ee3f4
seunghwan <seosh817@naver.com>
2024-09-27 23:23:16 +0900

    feat: Add repository for video data access

--- core/domain/src/main/kotlin/com/seosh817/moviehub/core/domain/repository/VideosRepository.kt
+++ core/domain/src/main/kotlin/com/seosh817/moviehub/core/domain/repository/VideosRepository.kt
@@ -13,10 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.seosh817.moviehub.core.model
+package com.seosh817.moviehub.core.domain.repository
 
-data class MovieDetailResult(
-    val movieDetail: MovieDetail,
-    val movieCredits: Credits,
-    val movieVideos: VideoResponse,
-)
+import com.seosh817.common.result.ResultState
+import com.seosh817.moviehub.core.model.VideoResponse
+
+interface VideosRepository {
+
+    suspend fun fetchVideos(movieId: Long): ResultState<VideoResponse>
+}
